ed6e54e583fa9b29d858d9598bed13870a253818
robinvd95 <robinvd95@live.nl>
2017-11-13 21:03:42 +0100

    Test commit
    Testing the commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,13 @@ namespace School
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int i = 0;
+            Increment.inc(ref i);
+            Increment.inc(ref i);
+            Increment.inc(ref i);
+            Increment.inc(ref i);
+            Increment.inc(ref i);
+            Console.WriteLine(i);
             Console.ReadKey();
         }
     }
---
+++ ConsoleApp1/ConsoleApp1/SecondClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School
+{
+    public static class Increment
+    {
+        public static void inc(ref int i)
+        {
+            i++;
+        }
+    }
+}
